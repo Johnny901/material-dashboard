7110689a803f062b02fad7825b370e105fe9bd0b
Johnny <johnny@xpand.group>
2020-07-31 16:24:59 +0100

    edit

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -96,7 +96,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Web-Portal"}
+        logoText={"Dashboard"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
